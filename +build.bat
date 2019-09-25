fda37864aa345df079c184f44cf068583a6170eb
Gilyazeev Adel <Atom735.ag47@gmail.com>
2019-09-25 14:35:02 +0300

    Add files via upload

--- +build.bat
+++ +build.bat
@@ -1,1 +1,11 @@
-"mingw32-make.exe"
+rem "mingw32-make.exe"
+gcc.exe^
+  -o main.exe^
+  -Wall -g^
+  -DWIN32_LEAN_AND_MEAN^
+  -D_WIN32_WINNT=_WIN32_WINNT_WIN7^
+  -DUNICODE^
+  src/main.c^
+  -mwindows -municode -m64
+
+main
