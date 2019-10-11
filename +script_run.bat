f0b3a5cf4ff944da335c6a56ffadf421cf41f723
Atom735 [kpfu-213] <Atom735.ag47@gmail.com>
2019-10-11 18:54:23 +0300

    SciLab plotting

--- +script_run.bat
+++ +script_run.bat
@@ -1,5 +1,26 @@
-start main -noGUI -new -N 2048 -dT "1e-5" -F 1000000 -S_t "0.9" -S_x "0.1" -a "3.0" -Err "1e-10" -IS 10 -Km "1" -txt "data/original.txt" "data/original.bin"
 
 
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_30_0.txt" "data/N9_4_100_30_0.bin"
 
-start main -noGUI -new -G -N 256 -dT "1e-3" -F 10000 -S_t "0.9" -S_x "0.1" -a "3.0" -IIM 4 -IS 10 -Km "1" -txt "data/orig_0.txt" "data/orig_0.bin"
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.1" -a "3.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_101_30_0.txt" "data/N9_4_101_30_0.bin"
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.2" -a "3.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_102_30_0.txt" "data/N9_4_102_30_0.bin"
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.3" -a "3.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_103_30_0.txt" "data/N9_4_103_30_0.bin"
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.4" -a "3.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_104_30_0.txt" "data/N9_4_104_30_0.bin"
+
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "4.5" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_45_0.txt" "data/N9_4_100_45_0.bin"
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "4.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_40_0.txt" "data/N9_4_100_40_0.bin"
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.5" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_35_0.txt" "data/N9_4_100_35_0.bin"
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "2.5" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_25_0.txt" "data/N9_4_100_25_0.bin"
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "2.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_20_0.txt" "data/N9_4_100_20_0.bin"
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "1.5" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_15_0.txt" "data/N9_4_100_15_0.bin"
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "1.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_10_0.txt" "data/N9_4_100_10_0.bin"
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "0.5" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_05_0.txt" "data/N9_4_100_05_0.bin"
+
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e1" -txt "data/N9_4_100_30_1.txt" "data/N9_4_100_30_1.bin"
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e2" -txt "data/N9_4_100_30_2.txt" "data/N9_4_100_30_2.bin"
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e3" -txt "data/N9_4_100_30_3.txt" "data/N9_4_100_30_3.bin"
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e4" -txt "data/N9_4_100_30_4.txt" "data/N9_4_100_30_4.bin"
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e-1" -txt "data/N9_4_100_30_-1.txt" "data/N9_4_100_30_-1.bin"
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e-2" -txt "data/N9_4_100_30_-2.txt" "data/N9_4_100_30_-2.bin"
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e-3" -txt "data/N9_4_100_30_-3.txt" "data/N9_4_100_30_-3.bin"
+start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e-4" -txt "data/N9_4_100_30_-4.txt" "data/N9_4_100_30_-4.bin"
--- sci.sce
+++ sci.sce
@@ -1,53 +1,37 @@
 szPathToData            = 'C:\ARGilyazeev\github\stud\data\';
-[fdData, err]           = mopen ( szPathToData + 'original.bin' );
-kI_Magic                = mget( 1, 'ul', fdData );
-kN_FrameCount           = mget( 1, 'u', fdData );
-kN_GridX                = mget( 1, 'u', fdData );
-kN_ItersIdentMi         = mget( 1, 'u', fdData );
-kN_ItersSkips           = mget( 1, 'u', fdData );
-kR_ErrorMax             = mget( 1, 'd', fdData );
-kR_Alpha                = mget( 1, 'd', fdData );
-kR_K_Mu                 = mget( 1, 'd', fdData );
-kR_dT                   = mget( 1, 'd', fdData );
-kR_S_t                  = mget( 1, 'd', fdData );
-kR_S_x                  = mget( 1, 'd', fdData );
-kR_dX                   = 1.0 / ( kN_GridX - 1.0 );
 
-function [ s, p ] = rDataLoadFrame ( i )
-  mseek ( 72 + i*kN_GridX*2*8, fdData );
-  s = mget( kN_GridX, 'd', fdData );
-  if i < kN_FrameCount then
-    p = mget( kN_GridX, 'd', fdData );
+
+function rDataPlot ( szFileName )
+  [fdData, err]           = mopen ( szPathToData + szFileName );
+  kI_Magic                = mget( 1, 'ul', fdData );
+  kN_FrameCount           = mget( 1, 'u', fdData );
+  kN_GridX                = mget( 1, 'u', fdData );
+  kN_ItersIdentMi         = mget( 1, 'u', fdData );
+  kN_ItersSkips           = mget( 1, 'u', fdData );
+  kR_ErrorMax             = mget( 1, 'd', fdData );
+  kR_Alpha                = mget( 1, 'd', fdData );
+  kR_K_Mu                 = mget( 1, 'd', fdData );
+  kR_dT                   = mget( 1, 'd', fdData );
+  kR_S_t                  = mget( 1, 'd', fdData );
+  kR_S_x                  = mget( 1, 'd', fdData );
+  kR_dX                   = 1.0 / ( kN_GridX - 1.0 );
+
+  function [ s, p ] = rDataLoadFrame ( i )
+    mseek ( 72 + i*kN_GridX*2*8, fdData );
+    s = mget( kN_GridX, 'd', fdData );
+    if i < kN_FrameCount then
+      p = mget( kN_GridX, 'd', fdData );
+    end;
+  endfunction
+
+  listX = 0:kR_dX:1;
+
+  gca().background = color(0.3,0.3,0.3);
+  for i = 1:10:kN_FrameCount
+      plot ( listX, rDataLoadFrame(i), "foreground",hsv2rgb(i/kN_FrameCount,1,1));
   end;
 endfunction
 
-listX = 0:kR_dX:1;
-
-gca().background = color(0.3,0.3,0.3);
-for i = 1:100:1500
-    plot ( listX, rDataLoadFrame(i), "foreground",hsv2rgb(i/1500,1,1));
-end;
-
-
-mclose ( fd );
-
-
-clf()
-gca().background = color(0.3,0.3,0.3);
-subplot(2,1,1)
-
-for i = 1:100:kN_FrameCount
-    plot(0:1/(kN_GridX-1):1,fP(i,:),"foreground",hsv2rgb(i/kN_FrameCount,1,1));
-end;
-title( "P(x)", "fontsize",3)
-gca().background = color(0.3,0.3,0.3);
-
-subplot(2,1,2)
-for i = 1:100:kN_FrameCount
-    plot(0:1/(kN_GridX-1):1,fS(i,:),"foreground",hsv2rgb(i/kN_FrameCount,1,1));
-end;
-title( "S(x)", "fontsize",3)
-gca().background = color(0.3,0.3,0.3);
-
-plot3d(0:1/(kN_FrameCount-1):1,0:1/(kN_GridX-1):1,fP);
-plot3d(0:1/(kN_FrameCount):1,0:1/(kN_GridX-1):1,fS);
+rDataPlot('N9_4_100_30_0.bin');
+rDataPlot('N9_4_102_30_0.bin');
+rDataPlot('N9_4_104_30_0.bin');
