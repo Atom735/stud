89c8e372354b55646ab3dccbc7db05904cd7aa4b
Atom735 [kpfu-213] <Atom735.ag47@gmail.com>
2019-10-22 21:05:58 +0300

    Start writing kypca4

--- +script_run.bat
+++ +script_run.bat
@@ -1,26 +1,45 @@
 
 
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_30_0.txt" "data/N9_4_100_30_0.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_30_0.txt" "data/N9_4_100_30_0.bin"
 
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.1" -a "3.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_101_30_0.txt" "data/N9_4_101_30_0.bin"
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.2" -a "3.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_102_30_0.txt" "data/N9_4_102_30_0.bin"
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.3" -a "3.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_103_30_0.txt" "data/N9_4_103_30_0.bin"
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.4" -a "3.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_104_30_0.txt" "data/N9_4_104_30_0.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.1" -a "3.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_101_30_0.txt" "data/N9_4_101_30_0.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.2" -a "3.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_102_30_0.txt" "data/N9_4_102_30_0.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.3" -a "3.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_103_30_0.txt" "data/N9_4_103_30_0.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.4" -a "3.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_104_30_0.txt" "data/N9_4_104_30_0.bin"
 
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "4.5" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_45_0.txt" "data/N9_4_100_45_0.bin"
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "4.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_40_0.txt" "data/N9_4_100_40_0.bin"
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.5" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_35_0.txt" "data/N9_4_100_35_0.bin"
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "2.5" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_25_0.txt" "data/N9_4_100_25_0.bin"
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "2.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_20_0.txt" "data/N9_4_100_20_0.bin"
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "1.5" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_15_0.txt" "data/N9_4_100_15_0.bin"
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "1.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_10_0.txt" "data/N9_4_100_10_0.bin"
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "0.5" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_05_0.txt" "data/N9_4_100_05_0.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "4.5" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_45_0.txt" "data/N9_4_100_45_0.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "4.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_40_0.txt" "data/N9_4_100_40_0.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.5" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_35_0.txt" "data/N9_4_100_35_0.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "2.5" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_25_0.txt" "data/N9_4_100_25_0.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "2.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_20_0.txt" "data/N9_4_100_20_0.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "1.5" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_15_0.txt" "data/N9_4_100_15_0.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "1.0" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_10_0.txt" "data/N9_4_100_10_0.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "0.5" -Err "1e-8" -Km "1e0" -txt "data/N9_4_100_05_0.txt" "data/N9_4_100_05_0.bin"
 
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e1" -txt "data/N9_4_100_30_1.txt" "data/N9_4_100_30_1.bin"
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e2" -txt "data/N9_4_100_30_2.txt" "data/N9_4_100_30_2.bin"
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e3" -txt "data/N9_4_100_30_3.txt" "data/N9_4_100_30_3.bin"
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e4" -txt "data/N9_4_100_30_4.txt" "data/N9_4_100_30_4.bin"
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e-1" -txt "data/N9_4_100_30_-1.txt" "data/N9_4_100_30_-1.bin"
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e-2" -txt "data/N9_4_100_30_-2.txt" "data/N9_4_100_30_-2.bin"
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e-3" -txt "data/N9_4_100_30_-3.txt" "data/N9_4_100_30_-3.bin"
-start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e-4" -txt "data/N9_4_100_30_-4.txt" "data/N9_4_100_30_-4.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e1" -txt "data/N9_4_100_30_1.txt" "data/N9_4_100_30_1.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e2" -txt "data/N9_4_100_30_2.txt" "data/N9_4_100_30_2.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e3" -txt "data/N9_4_100_30_3.txt" "data/N9_4_100_30_3.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e4" -txt "data/N9_4_100_30_4.txt" "data/N9_4_100_30_4.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e-1" -txt "data/N9_4_100_30_-1.txt" "data/N9_4_100_30_-1.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e-2" -txt "data/N9_4_100_30_-2.txt" "data/N9_4_100_30_-2.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e-3" -txt "data/N9_4_100_30_-3.txt" "data/N9_4_100_30_-3.bin"
+rem start main -noGUI -N 512 -dT "0.001" -F 2000 -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-8" -Km "1e-4" -txt "data/N9_4_100_30_-4.txt" "data/N9_4_100_30_-4.bin"
+
+
+
+start main -G -new ^
+    -N 1000 -dT "0.001" ^
+    -S_t "1.0" -S_x "0.0" -a "3.0" -Err "1e-5" -Km "1e-1" -txt ^
+    "data/_original.txt" "data/_original.bin" ^
+     -G -new ^
+    -N 1000 -dT "0.001" ^
+    -S_t "1.0" -S_x "0.2" -a "3.0" -Err "1e-5" -Km "1e-1" -txt ^
+    "data/_firstrules.txt" "data/_firstrules.bin" ^
+     -G -new ^
+    -N 1000 -dT "0.001" ^
+    -S_t "0.8" -S_x "0.0" -a "3.0" -Err "1e-5" -Km "1e-1" -txt ^
+    "data/_leftrules.txt" "data/_leftrules.bin" ^
+     -G -new ^
+    -N 1000 -dT "0.001" ^
+    -S_t "0.8" -S_x "0.2" -a "3.0" -Err "1e-5" -Km "1e-1" -txt ^
+    "data/_combo.txt" "data/_combo.bin" ^
--- sci.sce
+++ sci.sce
@@ -1,7 +1,8 @@
+clear
+mclose ( 'all' );
 szPathToData            = 'C:\ARGilyazeev\github\stud\data\';
 
-
-function rDataPlot ( szFileName )
+function rDataPlot ( szFileName, n, c )
   [fdData, err]           = mopen ( szPathToData + szFileName );
   kI_Magic                = mget( 1, 'ul', fdData );
   kN_FrameCount           = mget( 1, 'u', fdData );
@@ -26,12 +27,20 @@ function rDataPlot ( szFileName )
 
   listX = 0:kR_dX:1;
 
-  gca().background = color(0.3,0.3,0.3);
-  for i = 1:10:kN_FrameCount
+  gca().background = color(0.0,0.0,0.0);
+  for i = 1:n:kN_FrameCount
       plot ( listX, rDataLoadFrame(i), "foreground",hsv2rgb(i/kN_FrameCount,1,1));
+      // plot ( listX, rDataLoadFrame(i), "foreground", c );
   end;
+
+
+  mclose ( fdData );
 endfunction
 
-rDataPlot('N9_4_100_30_0.bin');
-rDataPlot('N9_4_102_30_0.bin');
-rDataPlot('N9_4_104_30_0.bin');
+a = 10;
+//rDataPlot ( '_original.bin', a, color(255.0,255.0,255.0) );
+//rDataPlot ( '_leftrules.bin', a, color(0.0,0.0,255.0) );
+rDataPlot ( '_firstrules.bin', a, color(0.0,255.0,0.0) );
+
+
+
